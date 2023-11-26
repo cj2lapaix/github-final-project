a3937863155b6651bea70e6b987bd9cd19552cad
Criss Ekoloko <cj2lapaix@gmail.com>
2023-11-25 19:31:39 -0500

    Functionalities completed

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/appointments", methods=["GET", "POST"])
+def appointments():
+    return render_template("appointments.html")
+
+@app.route("/registrants")
+def registration():
+    registrants = session.get("registrants", [])
+    return render_template("registrants.html", registrants=registrants)
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        phone = request.form.get("phone")
+        timefield = request.form.get("timefield")
+        date = request.form.get("date")
+        registrant = {'name': name, 'phone': phone, "timefield": timefield, "date": date}
+
+        # Get the existing registrants from the session or initialize an empty list
+        registrants = session.get("registrants", [])
+
+        # Append the new registrant
+        registrants.append(registrant)
+
+        # Update the session with the modified list of registrants
+        session["registrants"] = registrants
+
+    return render_template("success.html")
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+    if request.method == "POST":
+        # Get the index of the registrant to be deregistered
+        index_to_remove = int(request.form.get("id"))
+
+        # Get the existing registrants from the session or initialize an empty list
+        registrants = session.get("registrants", [])
+
+        if 1 <= index_to_remove <= len(registrants):
+            # Remove the registrant at the specified index
+            removed_registrant = registrants.pop(index_to_remove - 1)
+
+            # Update the session with the modified list of registrants
+            session["registrants"] = registrants
+
+            # Optionally, you can use the removed registrant data as needed
+            print(f"Deregistered: {removed_registrant}")
+
+    return redirect("/registrants")
+
+
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return render_template('session_cleared.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
